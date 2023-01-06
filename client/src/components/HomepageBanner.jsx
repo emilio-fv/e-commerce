2f66e53c7902bcce9cd4cdc71e8bc90cbfd6f6dd
emilio-fv <emiliofv.sd@gmail.com>
2023-01-05 20:54:22 -0700

    Replace banner

--- client/src/components/HomepageBanner.jsx
+++ client/src/components/HomepageBanner.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import ClothingBannerImg from '../images/banner.png';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ClothingBannerImg from '../images/model.jpg';
 
 const HomepageBanner = (props) => {
     return (
         <>
-            <Container disableGutters maxWidth="xl">
-                <Paper elevation={0}>
-                    <img src={ClothingBannerImg} alt="Homepage clothing display" className='BannerImg'/>
-                </Paper>
+            <Container disableGutters maxWidth="xl" sx={{ mt: 8, mb: 10,display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+                    <Typography textAlign='center' variant='h3' sx={{ mb: 3 }}>
+                        Luxury.<br/>
+                        Modern.<br/>
+                        Minimalist.<br/>
+                    </Typography>
+                    <Button>
+                        TODO: SHOP NOW
+                    </Button>
+                </Box>
+
+                <img src={ClothingBannerImg} alt="Homepage clothing display" className='BannerImg'/>
             </Container>
         </>
     )
